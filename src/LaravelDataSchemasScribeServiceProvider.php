e8349d1443db07374c6f2e0b8eb76b2e7ad73d31
Stephen Rushing <stephenr85@gmail.com>
2026-06-16 17:39:07 -0400

    chore: scaffold laravel-data-schemas-scribe bridge skeleton

---
+++ src/LaravelDataSchemasScribeServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Rushing\LaravelDataSchemasScribe;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelDataSchemasScribeServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
